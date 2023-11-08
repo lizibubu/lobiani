f0f5cdba07e0cf54f8de9d9fb15281282ee759b3
S <s>
2023-11-08 11:52:39 +0400

    {"types":"C","runs":8}

--- Problem07.java
+++ Problem07.java
@@ -1,4 +1,36 @@
+import stanford.karel.SuperKarel;
 
-public class Problem07 {
+public class Problem07 extends SuperKarel {
+	public void run() {
+		while(leftIsClear()){
+			fillaline();
+			goback();
+			if (leftIsClear()) {
+				turnLeft();
+				move();
+				turnRight();
+			}
+		}
+		fillOneLine();
+	}
 
+	private void goback() {
+		turnAround();
+		while (frontIsClear()) {
+			move();
+		}
+		turnAround();
+	}
+
+	private void fillaline() {
+		if (noBeepersPresent()) {
+			putBeeper();
+		}
+		while (frontIsClear()) {
+			move();
+			if (noBeepersPresent()) {
+				putBeeper();
+			}
+		}
+	}
 }
