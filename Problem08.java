4c63e37e1fb252792c459985c67e8bc89f156461
S <s>
2023-11-08 11:56:10 +0400

    {"types":"C","runs":10}

--- Problem08.java
+++ Problem08.java
@@ -1,4 +1,35 @@
+import stanford.karel.SuperKarel;
 
-public class Problem08 {
+public class Problem08 extends SuperKarel {
+	public void run() {
+		move();
+		transportAllBeepersToForth();
+		move();
+		minusBeepersOnNeighboringCells();
+	}
 
+	private void minusBeepersOnNeighboringCells() {
+		while (beepersPresent()) {
+			pickBeeper();
+			move();
+			pickBeeper();
+			turnAround();
+			move();
+			turnAround();
+		}
+
+	}
+
+	private void transportAllBeepersToForth() {
+		while (beepersPresent()) {
+			pickBeeper();
+			move();
+			move();
+			putBeeper();
+			turnAround();
+			move();
+			move();
+			turnAround();
+		}
+	}
 }
