f5da0a79ef62b0320e86738c160131b5a36d1c70
S <s>
2023-11-08 12:23:46 +0400

    {"types":"C","runs":14}

--- Problem11.java
+++ Problem11.java
@@ -1,4 +1,23 @@
+import stanford.karel.SuperKarel;
 
-public class Problem11 {
+public class Problem11 extends SuperKarel {
+	public void run() {
+		while (noBeepersPresent()) {
+			while (rightIsBlocked()) {
+				while (frontIsBlocked()) {
+					turnLeft();
+				}
+				if (noBeepersPresent()) {
+					move();
+				}
+				while (rightIsClear()) {
+					turnRight();
+					if (noBeepersPresent()) {
+						move();
+					}
+				}
 
+			}
+		}
+	}
 }
