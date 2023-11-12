cd3a2dff2ee4e22f4d0d8cf25cede2fa1deb7910
S <s>
2023-11-12 16:41:34 +0400

    {"types":"C","runs":30}

--- Problem14.java
+++ Problem14.java
@@ -2,8 +2,8 @@ import acm.graphics.*;
 import acm.program.*;
 
 public class Problem14 extends GraphicsProgram {
-	private static final int FACE_Y = 200;
-	private static final int FACE_X = 200;
+	private static final int FACE_Y = 400;
+	private static final int FACE_X = 400;
 	private static final int EYES_Y = 80;
 	private static final int EYES_X = 80;
 	private static final int MOUTH_Y = 30;
