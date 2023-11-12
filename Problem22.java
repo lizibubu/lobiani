871af6ae587161868c65a27adf95400b867cf4eb
S <s>
2023-11-12 17:53:03 +0400

    {"types":"C","runs":11}

--- Problem22.java
+++ Problem22.java
@@ -1,4 +1,20 @@
+import acm.graphics.*;
+import acm.program.*;
 
-public class Problem22 {
-
+public class Problem22 extends GraphicsProgram {
+	public void run() {
+		int xDis = getWidth() / 8;
+		int yDis = getHeight() / 8;
+		for (int i = 0; i < 8; i++) {
+			for (int j = 0; j < 8; j++) {
+				GRect rect = new GRect(xDis, yDis);
+				rect.setLocation( j * xDis , i * yDis );
+				if ((i + j) % 2 == 0) {
+					rect.setFilled(true);
+				}
+				add(rect);
+				println("i: " + i + " j: " + j + " -> " + (i * 8 + j));
+			}
+		}
+	}
 }
