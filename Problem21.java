ab73854f0bb87e646aa388318f63f7e165f2c1d5
S <s>
2023-11-12 17:47:17 +0400

    {"types":"C","runs":10}

--- Problem21.java
+++ Problem21.java
@@ -1,4 +1,25 @@
+import acm.graphics.*;
+import acm.program.*;
 
-public class Problem21 {
+public class Problem21 extends GraphicsProgram {
+	public void run() {
+		verticalLines();
+		horizontalLines();
+	}
 
+	private void horizontalLines() {
+		double dis = getHeight() / 10;
+		for (int i = 1; i < 10; i++) {
+			GLine horizontal = new GLine( 0, i * dis, getWidth(), i * dis );
+			add(horizontal);
+		}
+	}
+
+	private void verticalLines() {
+		double distance = getWidth() / 10;
+		for (int i = 1; i < 10; i++) {
+			GLine vertical = new GLine( i * distance, 0, i * distance, getHeight());
+			add(vertical);
+		}
+	}
 }
