6b3b6e657373f11fda9fc0446cb6f6242cc5a320
S <s>
2023-11-12 19:08:18 +0400

    {"types":"C","runs":29}

--- Problem25.java
+++ Problem25.java
@@ -4,7 +4,7 @@ public class Problem25 extends ConsoleProgram {
 	public void run() {
 		int a = readInt ("first number: ");
 		int b = readInt ("second number: ");
-		for ( int i = 0; i <= a * b; i++ ) {
+		for ( int i = 1; i <= a * b; i++ ) {
 			if ( i % a == 0 && i % b == 0 ) {
 				println (i);
 				break;
