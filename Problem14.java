3237b38b507432b1a4d7486996e91525687ad907
S <s>
2023-11-12 16:38:10 +0400

    {"types":"C","runs":26}

--- Problem14.java
+++ Problem14.java
@@ -1,4 +1,21 @@
+import acm.graphics.*;
+import acm.program.*;
 
-public class Problem14 {
-
+public class Problem14 extends GraphicsProgram {
+	private static final int FACE_Y = 200;
+	private static final int FACE_X = 200;
+	private static final int EYES_Y = 80;
+	private static final int EYES_X = 80;
+	private static final int MOUTH_Y = 30;
+	private static final int MOUTH_X = 70;
+	public void run() {
+		drawFace();
+		drawEyes();
+		drawMouth();
+	}
+	private void drawFace() {
+		GRect face = new GRect ( FACE_X , FACE_Y );
+		add ( face , getWidth() / 2 - FACE_X / 2 , getHeight() / 2 - FACE_Y / 2);
+		
+	}
 }
