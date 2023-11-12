1b2e12d985f52069668d2fe0cea7afbb6805c4bc
S <s>
2023-11-12 17:08:23 +0400

    {"types":"C","runs":1}

--- Problem16.java
+++ Problem16.java
@@ -1,4 +1,12 @@
+import acm.program.ConsoleProgram;
 
-public class Problem16 {
+public class Problem16 extends ConsoleProgram {
+	public void run() {
+		int a = readInt("First number: ");
+		int b = readInt("Second number: ");
+		int c = readInt("Third number: ");
+		double harmonic = 1 / (1 / (double)a + 1.0/ b + (double)1 / c);
+		println(harmonic);
+	}
 
 }
