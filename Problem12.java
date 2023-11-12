0a2ad2b7b2ed4635352c1e4355b34c6e296620d4
S <s>
2023-11-12 16:09:06 +0400

    {"types":"C","runs":13}

--- Problem12.java
+++ Problem12.java
@@ -13,8 +13,10 @@ public class Problem12 extends GraphicsProgram {
 		
 	}
 	private void drawRoof() {
-		GLine line1 = new GLine ( getWidth() / 2 - HOUSE_X / 2 , getHeight() - HOUSE_Y ,getWidth() / 2 , HOUSE_Y - 50 );
+		GLine line1 = new GLine ( getWidth() / 2 - HOUSE_X / 2 , getHeight() - HOUSE_Y , getWidth() / 2 , HOUSE_Y - 50 );
 		add (line1);
+		GLine line2 = new GLine ( getWidth() /2 + HOUSE_X / 2 , getHeight() - HOUSE_Y , getWidth() / 2 , HOUSE_Y - 50 );
+		add (line2);
 	}
 	private void drawWalls() {
 		GRect walls = new GRect (HOUSE_X , HOUSE_Y );
