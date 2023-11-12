211548e6256dea84dcabc9220ec6e75a74527e13
S <s>
2023-11-12 17:17:46 +0400

    {"types":"C","runs":3}

--- Problem18.java
+++ Problem18.java
@@ -1,4 +1,17 @@
+import acm.program.ConsoleProgram;
 
-public class Problem18 {
+public class Problem18 extends ConsoleProgram {
+	public void run() {
+		int a = readInt ("Number: ");
+		int b = readInt ("Another number: ");
+		int max = maximum (a, b);
+		println (max);
+	}
 
+	private int maximum(int a, int b) {
+		if ( a > b ) {
+			return a;
+		}
+		return b;
+	}
 }
