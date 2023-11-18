69dcd48b9f1d2d2cea46813ce53c100f8b846471
S <s>
2023-11-18 23:09:57 +0400

    {"types":"C","runs":1}

--- Problem26.java
+++ Problem26.java
@@ -1,4 +1,18 @@
+import acm.program.ConsoleProgram;
 
-public class Problem26 {
-
+public class Problem26 extends ConsoleProgram {
+	public void run() {
+		int a = readInt ("first number: ");
+		int b = readInt ("second number: ");
+		int small = a;
+		if (b < a) {
+			small = b;
+		}
+		while (small > 0 ) {
+			if ( a % small == 0 && b % small == 0 ) {
+				println(small);
+				break;
+			}
+		}
+	}
 }
