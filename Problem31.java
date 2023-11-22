35206473770a9aceefa2a5e21db455a079ea63af
S <s>
2023-11-22 23:25:25 +0400

    {"types":"C","runs":8}

--- Problem31.java
+++ Problem31.java
@@ -1,4 +1,19 @@
+import acm.program.ConsoleProgram;
 
-public class Problem31 {
-
+public class Problem31 extends ConsoleProgram {
+	public void run() {
+		int a = readInt ("enter a: ");
+		int div = 0;
+		int n = readInt ("gamyofebi");
+		for (int i = 0; i <= n; i++) {
+			if ( n % i == 0) {
+				div ++; 
+			}
+		}
+		if ( n == 2 ) {
+			println ("prime");
+		} else {
+			println ("not prime");
+		}
+	}
 }
