31c2deb325a27007638e87fe8b5468797859085f
S <s>
2023-11-22 23:26:09 +0400

    {"types":"C","runs":9}

--- Problem31.java
+++ Problem31.java
@@ -4,13 +4,12 @@ public class Problem31 extends ConsoleProgram {
 	public void run() {
 		int a = readInt ("enter a: ");
 		int div = 0;
-		int n = readInt ("gamyofebi");
-		for (int i = 0; i <= n; i++) {
-			if ( n % i == 0) {
+		for (int i = 0; i <= a; i++) {
+			if ( a % i == 0) {
 				div ++; 
 			}
 		}
-		if ( n == 2 ) {
+		if ( div == 2 ) {
 			println ("prime");
 		} else {
 			println ("not prime");
