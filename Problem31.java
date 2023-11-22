8d709bb1d5df00650560daac9a671c94f0a87205
S <s>
2023-11-22 23:47:03 +0400

    {"types":"C","runs":22}

--- Problem31.java
+++ Problem31.java
@@ -3,15 +3,21 @@ import acm.program.ConsoleProgram;
 public class Problem31 extends ConsoleProgram {
 	public void run() {
 		int n = readInt ("enter a number: ");
+		int div = div(n);
+		if ( div == 2 ) {
+			println ("prime");
+		} else {
+			println ("not prime");
+		}
+	}
+
+	private int div(int n) {
 		int div = 0;
 		for (int i = 0; i <= n; i++) {
 			if ( n % i == 0) {
 				div ++; 
 			}
 		}
-		println (div);
-		if ( div == 2 ) {
-			println ("prime");
-		} 
+		return div;
 	}
 }
