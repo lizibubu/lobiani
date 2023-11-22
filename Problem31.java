9172860e14cd3598823b1ae254afc9eb8ab44aae
S <s>
2023-11-22 23:27:09 +0400

    {"types":"A","runs":11}

---
+++ Problem31.java
@@ -0,0 +1,14 @@
+import acm.program.ConsoleProgram;
+
+public class Problem31 extends ConsoleProgram {
+	public void run() {
+		int n = readInt ("enter a number: ");
+		int div = 0;
+		for (int i = 0; i <= n; i++) {
+			if ( n % i == 0) {
+				div ++; 
+			}
+		}
+				
+	}
+}
