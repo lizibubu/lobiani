17fd96b45dc936502471a5808bdb05d7db5cb186
S <s>
2023-11-22 22:53:56 +0400

    {"types":"C","runs":1}

--- Problem29.java
+++ Problem29.java
@@ -1,4 +1,20 @@
+import acm.program.ConsoleProgram;
 
-public class Problem29 {
+public class Problem29 extends ConsoleProgram {
+	public void run() {
+		int n = readInt ("enter n: ");
+		int an = readFib(n);
+	}
+
+	private int readFib(int n) {
+		int a1 = 1;
+		int a2 = 1;
+		for (int i = 2; i < n; i++ ) {
+			int sum = a1 + a2 ;
+			a1 = a2;
+			a2 = sum;
+		}
+		return a2;
+	}
 
 }
