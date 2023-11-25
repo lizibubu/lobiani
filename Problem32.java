bcfe54cafbb5c39d525c8c686e56a4904aaa0dc9
S <s>
2023-11-25 23:48:14 +0400

    {"types":"C","runs":9}

--- Problem32.java
+++ Problem32.java
@@ -2,7 +2,7 @@ import acm.program.ConsoleProgram;
 
 public class Problem32 extends ConsoleProgram {
 	public void run() {
-		for ( int j = 1; j <= 20; j++ ) {	
+		for ( int j = 1; j <= 1000000; j++ ) {	
 			int div = 0;
 			for (int i = 1; i <= j; i++) {
 				if ( j % i == 0) {
