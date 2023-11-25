2b41351ea7a08dbd4843ac93b9d6328f21a5dfb1
S <s>
2023-11-26 00:03:34 +0400

    {"types":"C","runs":16}

--- Problem33.java
+++ Problem33.java
@@ -13,9 +13,9 @@ public class Problem33 extends GraphicsProgram {
 	}
 
 	private void makeRandCircle() {
-		int r = rgen.nextInt(0, getWidth() / 2);
+		int r = rgen.nextInt( 0, getWidth() / 2 );
 		Color color = rgen.nextColor();
-		GOval oval = new GOval(r * 2, r * 2);
+		GOval oval = new GOval( r * 2, r * 2 );
 		oval.setFilled(true);
 		oval.setFillColor(color);
 		add(oval);
