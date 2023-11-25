9d7b9ca27e66e4af2c6fc320abe7ff3c48980734
S <s>
2023-11-25 23:43:52 +0400

    {"types":"C","runs":7}

--- Problem32.java
+++ Problem32.java
@@ -2,19 +2,14 @@ import acm.program.ConsoleProgram;
 
 public class Problem32 extends ConsoleProgram {
 	public void run() {
-		for ( int j = 1; j <= 1000000; j++ ) {
-		int n = readInt ("start: ");
-		int div = 0;
-		for (int i = 1; i <= n; i++) {
-			if ( n % i == 0 ) {
-				div ++;
+		for ( int j = 1; j <= 1000000; j++ ) {	
+			int div = 0;
+			for (int i = 1; i <= j; i++) {
+				if ( j % i == 0) {
+					div ++; 
+				}
 			}
-		}
-			if ( div == 2 ) {
-			println ("prime");
-			} else {
-			println ("not prime");
-			}
-		}
-	}		
+			println (j);
+		}		
+	}
 }
