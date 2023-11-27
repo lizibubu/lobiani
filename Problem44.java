816ab9fe6257dea683d697eeedfd84373cec1b34
S <s>
2023-11-27 15:01:17 +0400

    {"types":"C","runs":30}

--- Problem44.java
+++ Problem44.java
@@ -1,4 +1,29 @@
+import java.awt.event.MouseEvent;
 
-public class Problem44 {
+import acm.graphics.GOval;
+import acm.program.GraphicsProgram;
 
+public class Problem44 extends GraphicsProgram {
+	private GOval ball = null;
+
+	public void run() {
+		addMouseListeners();
+		while (true) {
+			if (ball != null) {
+				ball.move(0, 3);
+			}
+			pause(100);
+		}
+	}
+
+	public void mouseClicked(MouseEvent e) {
+		if (ball == null) {
+			ball = new GOval(50, 50);
+			ball.setFilled(true);
+			ball.setLocation(e.getX(), e.getY());
+			add(ball);
+		} else {
+			ball.setLocation(e.getX(), e.getY());
+		}
+	}
 }
