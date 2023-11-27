fc90cf9e57a5184e683d6794186bc63ac5b63ab5
S <s>
2023-11-27 12:37:41 +0400

    {"types":"C","runs":9}

--- Problem38.java
+++ Problem38.java
@@ -1,4 +1,38 @@
+import acm.program.ConsoleProgram;
+import acm.util.RandomGenerator;
 
-public class Problem38 {
-
+public class Problem38 extends ConsoleProgram {
+	
+	private RandomGenerator random;
+	private static final int START_BALANCE = 1000;
+	
+	public void run() {
+		random = RandomGenerator.getInstance();
+		int balance = START_BALANCE;
+		while (balance > 0) {
+			println("you have " + balance );
+			int bet = readInt("enter bet amount: ");
+			if (bet > balance) {
+				println("not enough money, try again.");
+				continue;
+			}
+			int number = readInt("enter number: ");
+			balance += result(number, bet);
+		}
+	}
+	
+	private int result(int chosenNumber, int bet) {
+		int spinnedNumber = spin();
+		println(".... " + spinnedNumber);
+		if (spinnedNumber == chosenNumber) {
+			println("you won");
+			return 35 * bet;
+		}
+		println("you lose");
+		return -bet;
+	}
+	
+	private int spin() {
+		return random.nextInt(0, 36);
+	}
 }
