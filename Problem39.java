f9e9a572d86a2219005366f0dc33884d2af99519
S <s>
2023-11-27 13:29:17 +0400

    {"types":"C","runs":3}

--- Problem39.java
+++ Problem39.java
@@ -5,5 +5,10 @@ public class Problem39 extends GraphicsProgram {
 	public void run() {
 		GOval oval = new GOval(50, 50);
 		add (oval, 0, getHeight() / 2 - 50 );
+		double xv = 0;
+		double yv = 10;
+		while (oval.getX() + oval.getWidth() <= getWidth()) {
+			oval.move(xv, yv);
+		}
 	}
 }
