e22184bb55fbccd0d4536ae1b35a7ebec634815f
S <s>
2023-11-27 16:53:23 +0400

    {"types":"C","runs":1}

--- Problem45.java
+++ Problem45.java
@@ -1,4 +1,20 @@
+import acm.program.ConsoleProgram;
 
-public class Problem45 {
+public class Problem45 extends ConsoleProgram{
+	public void run(){
+		String w = readLine("Enter Word: ");
+		String l = readLine("Enter Letter: ");
+		int ans = countLetter(w, l.charAt(0));
+		print(ans);
+	}
 
+	private int countLetter(String word, char ch) {
+		int ans = 0;
+		for(int i = 0; i < word.length(); i++){
+			if(word.charAt(i) == ch){
+				ans++;
+			}
+		}
+		return ans;
+	}
 }
