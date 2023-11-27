633e34b370aaf8dabc3c81a4b6876026dd7b6f64
S <s>
2023-11-27 17:38:55 +0400

    {"types":"C","runs":5}

--- Problem47.java
+++ Problem47.java
@@ -2,7 +2,24 @@ import acm.program.ConsoleProgram;
 
 public class Problem47 extends ConsoleProgram {
 	public void run() {
-		//String word =
+		String word = "Enter Word: ";
+		if (isPalindrom(word)) {
+			println("palindromia");
+		} else {
+			println("ar aris palindromi");
+		}
+		
+	}
+
+	private boolean isPalindrom(String word) {
+		for (int i = 0; i < word.length(); i++) {
+			char first = word.charAt(i);
+			char last = word.charAt(word.length() - 1 - i);
+			if ( first == last ) {
+				return true;
+			}
+		}
+		return false;
 	}
 
 }
