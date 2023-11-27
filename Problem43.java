33097ae9d46907072e0d8503c82836c846762646
S <s>
2023-11-27 14:56:16 +0400

    {"types":"C","runs":26}

--- Problem43.java
+++ Problem43.java
@@ -1,4 +1,19 @@
+import java.awt.event.MouseEvent;
+import acm.graphics.GOval;
+import acm.program.GraphicsProgram;
 
-public class Problem43 {
+public class Problem43 extends GraphicsProgram {
+	private GOval oval = null;
+	public void run() {
+		oval = new GOval(50, 50);
+		oval.setFilled(true);
+		addMouseListeners();
+	}
+	
+	public void mouseDragged (MouseEvent e) {
+		if (getElementAt(e.getX(), e.getY()) == oval) {
+			oval.setLocation(e.getX(), e.getY());
+		}
+	}
 
 }
