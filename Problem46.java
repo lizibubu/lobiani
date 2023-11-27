3ab462b4f14fa3ed2005ce62fa8523b606f77fb1
S <s>
2023-11-27 17:14:33 +0400

    {"types":"C","runs":1}

--- Problem46.java
+++ Problem46.java
@@ -1,4 +1,32 @@
+import acm.program.ConsoleProgram;
 
-public class Problem46 {
+public class Problem46 extends ConsoleProgram{
+	public void run(){
+		String w = readLine("Enter Word: ");	
+		char ans = countMax(w);
+		print(ans);
+	}
 
+	private char countMax(String w) {
+		int max = 0;
+		char maxCh = ' ';
+		for(int i = 0; i < w.length(); i++){
+			int amount = countLetter(w, w.charAt(i));
+			if(amount > max){
+				max = amount;
+				maxCh = w.charAt(i);
+			}
+		}
+		return maxCh;
+	}
+
+	private int countLetter(String w, char ch) {
+		int ans = 0;
+		for(int i = 0; i < w.length(); i++){
+			if(w.charAt(i) == ch){
+				ans++;
+			}
+		}
+		return ans;
+	}
 }
