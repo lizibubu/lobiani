a4d61a7b4c048342183f1d5aa2bfc992a9db615b
S <s>
2023-11-27 19:11:29 +0400

    {"types":"C","runs":13}

--- Problem48.java
+++ Problem48.java
@@ -3,11 +3,19 @@ import acm.program.ConsoleProgram;
 public class Problem48 extends ConsoleProgram {
 	public void run() {
 		String word = readLine("enter word: ");
-		showStatistics();
+		showStatistics(word);
 	}
 
-	private void showStatistics() {
-		for (int i = 0; i < 5; i++ ) {
+	private void showStatistics(String word) {
+		for (int i = 0; i < 26; i++ ) {
+			char w = (char) ('a' + i);
+			int sum = 0;
+			for(int j = 0; j < word.length(); j++){
+				if(word.charAt(j) == w){
+					sum++;
+				}
+			}
+			println(w + sum);
 			
 		}
 		
