d858cfcf47b366d2a61c813dd0adcd1863dd1eb8
S <s>
2023-11-27 14:39:36 +0400

    {"types":"C","runs":21}

--- Problem42.java
+++ Problem42.java
@@ -1,4 +1,42 @@
+ import java.awt.event.MouseEvent;
 
-public class Problem42 {
+import acm.graphics.GLine;
+import acm.program.*;
+
+public class Problem42 extends GraphicsProgram {
+	private int countClick = 0;
+	private GLine line = null;
+	private int startX = -1;
+	private int startY = -1;
+	private boolean finished = false;
+	public void run() {
+		addMouseListeners();
+	}
+	
+	public void mouseMoved(MouseEvent e){
+		if(line != null && !finished){
+			remove(line);
+			line = new GLine(startX, startY, e.getX(), e.getY());
+			add(line);
+		}
+	}
+	
+	public void mouseClicked(MouseEvent e){
+		countClick++;
+		if(countClick == 1){
+			startX = e.getX();
+			startY = e.getY();
+			line = new GLine(startX, startY,
+					startX + 1, 
+					startY + 1
+					);
+			
+			add(line);
+		}else if(countClick == 2){
+			finished = true;
+		}
+	}
+	
+	
 
 }
