39897e99a08050ef643273cb293410d05188cc91
S <s>
2023-11-27 13:18:36 +0400

    {"types":"C","runs":2}

--- Problem39.java
+++ Problem39.java
@@ -4,6 +4,6 @@ import acm.program.GraphicsProgram;
 public class Problem39 extends GraphicsProgram {
 	public void run() {
 		GOval oval = new GOval(50, 50);
-		add (oval, 0, getHeight() / 2 + 50 );
+		add (oval, 0, getHeight() / 2 - 50 );
 	}
 }
