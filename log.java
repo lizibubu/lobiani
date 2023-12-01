d77f29afdd71e25873dceec42479bf7762892ff6
S <s>
2023-12-01 15:39:57 +0400

    {"types":"C","runs":1}

--- log.java
+++ log.java
@@ -1,4 +1,21 @@
+import acm.program.ConsoleProgram;
 
-public class log {
+public class log extends ConsoleProgram {
+	public void run() {
+		int n = readInt("Enter n: ");
+		int m = readInt ("Enter m: ");
+		println(logar(n, m));
+	}
 
-}
+	private int logar(int n, int m) {
+		int ans = 0;
+		for (int i = n; i < m; i*=n) {
+			ans ++;
+			if ( m == i ) {
+				return ans;
+			}
+		}
+		return 0;
+	}
+}	
+		
