c110c7793232ae43a6a0d4fd2c1f960a06d7fba9
S <s>
2023-12-02 11:57:52 +0400

    {"types":"C","runs":24}

--- exercise4.java
+++ exercise4.java
@@ -1,4 +1,34 @@
+import acm.program.ConsoleProgram;
 
-public class exercise4 {
+public class exercise4 extends ConsoleProgram {
+	public void run() {
+		String w = readLine("enter text: ");
+		char ans = countMax(w);
+		print(ans);
+		}
 
+
+	private char countMax(String w) {
+		int max = 0;
+		char maxch = ' ';
+		for (int i = 0; i < w.length(); i++) {
+			int amount = countLetter(w, w.charAt(i));
+			if (amount > max) {
+				max = amount;
+				maxch = w.charAt(i);
+			}
+		}
+		return maxch;
+	}
+
+
+	private int countLetter(String w, char ch) {
+		int ans = 0;
+		for (int i = 0; i < w.length(); i++) {
+			if (w.charAt(i) == ch) {
+				ans++;
+			}
+		}
+		return ans;
+	}
 }
