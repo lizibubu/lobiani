0337db636e5ee33de7bac2834e7c4ecfe0d50ccb
S <s>
2023-12-02 11:18:56 +0400

    {"types":"C","runs":17}

--- exercise3.java
+++ exercise3.java
@@ -1,4 +1,19 @@
+import java.awt.event.MouseEvent;
 
-public class exercise3 {
+import acm.graphics.GOval;
+import acm.program.GraphicsProgram;
+
+public class exercise3 extends GraphicsProgram {
+	private GOval oval = null;
+	public void run() {
+		oval = new GOval (100, 100);
+		oval.setFilled(true);
+		add(oval, getWidth() / 2 , getHeight() / 2);
+		addMouseListeners();
+		oval.move(0, 3);
+	}
+	public void mouseClicked(MouseEvent e) {
+		
+	}
 
 }
