df0d0cb06c534a7d20a6e2b1670816588e9f9c18
S <s>
2023-12-02 10:55:15 +0400

    {"types":"C","runs":1}

--- exercise1.java
+++ exercise1.java
@@ -1,4 +1,12 @@
+import acm.program.ConsoleProgram;
 
-public class exercise1 {
-
+public class exercise1 extends ConsoleProgram {
+	public void run() {
+		int n = readInt("Enter n: ");
+		int m = readInt("Enter m: ");
+		for ( int i = 1; i <= m; i++) {
+			n *= n;
+			println(n);
+		}
+	}
 }
