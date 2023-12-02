90bc2f4c88be0028f16ee414054f7f72ca7d6fe3
S <s>
2023-12-02 11:08:03 +0400

    {"types":"C","runs":9}

--- exercise2.java
+++ exercise2.java
@@ -1,4 +1,20 @@
+import java.awt.event.MouseEvent;
 
-public class exercise2 {
+import acm.graphics.GOval;
+import acm.program.GraphicsProgram;
+
+public class exercise2 extends GraphicsProgram {
+	private GOval oval = null;
+	public void run() {
+		oval = new GOval (100, 100);
+		add(oval, getWidth() / 2, getHeight() / 2);
+		addMouseListeners();
+	}
+	
+	public void mouseDragged(MouseEvent e) {
+		if (getElementAt(e.getX(), e.getY()) == oval ){
+			oval.setLocation(e.getX() - 10, e.getY() - 10 );
+		}
+	}
 
 }
