0dc9ad47ed8410ca203b4a515f73aa8da4b8f582
S <s>
2024-01-20 23:59:11 +0400

    {"types":"C","runs":1}

--- Problem52.java
+++ Problem52.java
@@ -1,4 +1,24 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
-public class Problem52 {
+import acm.program.ConsoleProgram;
 
+public class Problem52 extends ConsoleProgram {
+	public void run() {
+		try {
+			BufferedReader rder = new BufferedReader(
+						new FileReader("Seminari52.java")
+					);
+			while(true){
+				String line = rder.readLine();
+				if(line == null){
+					break;
+				}
+				println(line);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
