90deaf341900d0c8144a30d89ba7c3442528a1dc
S <s>
2024-01-21 00:08:04 +0400

    {"types":"C","runs":3}

--- Problem58.java
+++ Problem58.java
@@ -1,4 +1,28 @@
+import acm.program.ConsoleProgram;
 
-public class Problem58 {
+public class Problem58 extends ConsoleProgram{
+	public void run(){
+		
+	}
+	
+	private boolean isMagic(int[][] m){
+		int n = m.length;
+		for(int i = 1; i <= n; i++){
+			if(!containsMatrix(m, i)){
+				return false;
+			}
+		}
+		return true;
+	}
 
+	private boolean containsMatrix(int[][] m, int num) {
+		for(int i = 0; i < m.length; i++){
+			for(int j = 0; j < m[0].length; j++){
+				if(m[i][j] == num){
+					return true;
+				}
+			}
+		}
+		return false;
+	}
 }
