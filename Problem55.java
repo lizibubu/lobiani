4d5870689583774c35ce3d4cefe9854676d1c9f3
S <s>
2024-01-21 00:04:52 +0400

    {"types":"C","runs":3}

--- Problem55.java
+++ Problem55.java
@@ -1,4 +1,25 @@
+import acm.program.ConsoleProgram;
 
-public class Problem55 {
+public class Problem55 extends ConsoleProgram{
+	public void run(){
+		int n = readInt("Enter Amount: ");
+		int [] arr = new int[n];
+		fillArray(arr);
+		println(isSorted(arr));
+	}
+	
+	private boolean isSorted(int[] arr) {
+		for(int i = 1; i < arr.length; i++){
+			if(arr[i] < arr[i - 1]){
+				return false;
+			}
+		}
+		return true;
+	}
 
+	private void fillArray(int[] arr) {
+		for(int i = 0; i < arr.length; i++){
+			arr[i] = readInt("Enter Number: ");
+		}
+	}
 }
