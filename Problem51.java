24e438fef05292bb0639d84ee5f8e80bf20b0c20
S <s>
2024-01-20 23:54:01 +0400

    {"types":"C","runs":1}

--- Problem51.java
+++ Problem51.java
@@ -1,4 +1,15 @@
+import java.util.StringTokenizer;
+import acm.program.ConsoleProgram;
 
-public class Problem51 {
-
+public class Problem51 extends ConsoleProgram {
+	public void run() {
+		String text = readLine ("Enter text: ");
+		StringTokenizer tokenizer = new StringTokenizer(text);
+		int counter = 0;	
+		while(tokenizer.hasMoreTokens()){
+			counter ++;
+			println(tokenizer.nextToken());
+		}
+		println(counter);
+	}
 }
