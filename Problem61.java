e73c0eca06b8469e3bac22a28f1ebea6cf472467
S <s>
2024-01-22 13:51:44 +0400

    {"types":"C","runs":2}

--- Problem61.java
+++ Problem61.java
@@ -1,4 +1,20 @@
+import java.util.ArrayList;
+import acm.program.ConsoleProgram;
 
-public class Problem61 {
-
+public class Problem61 extends ConsoleProgram {
+	public void run(){
+		
+	}
+	
+	public void findSumCouples(ArrayList<Integer> list, int sum){
+		for(int i = 0; i < list.size(); i++){
+			for(int j = i + 1; j < list.size(); j++){
+				int a = list.get(i);
+				int b = list.get(j);
+				if(a + b == sum){
+					println(a + "-" + b);
+				}
+			}
+		}
+	}
 }
